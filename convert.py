a306eee3112d66167ab3dc7d4a114cc2bfd91c06
Markus Rudy <markus.rudy@sap.com>
2017-12-05 22:01:13 +0100

    initial bugfixes

--- convert.py
+++ convert.py
@@ -7,27 +7,33 @@ from collections import defaultdict
 # example format: 20170913_174448.jpg
 
 _filename_re = re.compile(r"([0-9]{4})([0-9]{2}).*")
-_conversion = "vips resize '{src}' '{dst}' 0.01"
-_copy = "cp '{src}' '{dst}'"
+_conversion = "vips resize '{src}' '{dst}' {scale:.03f}"
+_copy = "ln '{src}' '{dst}'"
+
 
 
 def name_to_dict(name):
     m = _filename_re.match(name)
     if m is None:
         return None
-    return dict(year=m.groups[0], month=m.groups[1], name=m.string)
+    g = m.groups()
+    return dict(year=g[0], month=g[1], name=m.string)
 
 
 def dict_to_out(base, ext, d):
     return os.path.join(base, d["year"], d["month"], d["name"] + ext)
 
 
-def convert(src, dst):
-    os.system(_conversion(src=src, dst=dst))
+def convert(src, dst, scale):
+    cmd = _conversion.format(src=src, dst=dst, scale=scale)
+    os.system(cmd)
 
 
 def copy(src, dst):
-    os.system(_copy(src=src, dst=dst))
+    os.system(_copy.format(src=src, dst=dst))
+
+def mkdir(target):
+    os.system("mkdir -p '{}'".format(target))
 
 
 if __name__ == "__main__":
@@ -41,16 +47,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     files = list(glob(os.path.join(args.input_dir, "*.jpg")))
-    num_items = len(l)
+    num_items = len(files)
     i = 0
 
     for f in files:
         print("processing {}/{}".format(i, num_items))
         i += 1
-        if i > 3:
-            break
         d = name_to_dict(os.path.basename(f))
         if d is None:
             raise ValueError("unexpected file name {}".format(f))
-        copy(f, dict_to_out(args.input_dir, "", f))
-        convert(f, dict_to_out(args.input_dir, ".small.jpg", f))
+        mkdir(os.path.dirname(dict_to_out(args.output_dir, "", d)))
+        copy(f, dict_to_out(args.output_dir, "", d))
+        convert(f, dict_to_out(args.output_dir, ".small.jpg", d), 0.01)
+        convert(f, dict_to_out(args.output_dir, ".medium.jpg", d), 0.05)
